81df29b6c13441fc3792768186b557358cc89c6c
Maksym Polinka <Polinka7max@gmail.com>
2019-08-13 09:49:09 +0300

    Added a .js files

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,6 +1,16 @@
 angular.module("login_form",[])
     .controller("AppCtrl", function ($scope, $http) {
         $scope.auth = {};
+        let resultMessageEl = document.getElementById('resultMessage');
+        let exampleInputNameEl = document.getElementById('exampleInputName');
+        let exampleInputLoginEl = document.getElementById('exampleInputLogin');
+        let inputNameLabel = document.getElementById('inputNameLabel');
+        let inputLoginLabel = document.getElementById('inputLoginLabel');
+        exampleInputNameEl.addEventListener('input', () => {
+            inputNameLabel.style.color = 'black';
+        inputLoginLabel.style.color = 'black';
+        $scope.message = '';
+        });
         $scope.sendForm = function(auth){
             $http({
                 method: "POST",
